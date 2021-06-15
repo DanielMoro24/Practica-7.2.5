e8eb6e39ac07abfb09dbd658335dcf77fcb11642
Daniel <daniel.moro@live.u-tad.com>
2021-06-15 09:54:30 +0200

    video 22

--- src/es/pildoras/ioC/ComercialExperimentado.java
+++ src/es/pildoras/ioC/ComercialExperimentado.java
@@ -1,11 +1,26 @@
 package es.pildoras.ioC;
 
-import org.springframework.context.annotation.Scope;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class ComercialExperimentado implements Empleados {
+
+	// ejecución de código después de cración del Bean
+
+	@PostConstruct
+	public void ejecutaDespuesCreacion() {
+		System.out.println("ejecuta tras creacion del bean");
+	}
+
+	// ejecución de código después de apagado contenedor Spring
+
+	@PreDestroy
+	public void ejecutaAntesDestruccion() {
+		System.out.println("ejecutando antes de la destruccion");
+	}
 
 	public ComercialExperimentado() {
 
