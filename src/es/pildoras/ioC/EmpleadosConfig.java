31badd8886d2fd9a7a80670fc8f36c804cd68005
Daniel <daniel.moro@live.u-tad.com>
2021-06-15 10:07:48 +0200

    video 23

---
+++ src/es/pildoras/ioC/EmpleadosConfig.java
@@ -0,0 +1,10 @@
+package es.pildoras.ioC;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("es.pildoras.ioC")
+public class EmpleadosConfig {
+
+}
--- src/es/pildoras/ioC/UseAnnotation2.java
+++ src/es/pildoras/ioC/UseAnnotation2.java
@@ -1,12 +1,18 @@
 package es.pildoras.ioC;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UseAnnotation2 {
 
 	public static void main(String[] args) {
 
-		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// ClassPathXmlApplicationContext contexto = new
+		// ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// leer el class de configuracion
+
+		AnnotationConfigApplicationContext contexto = new AnnotationConfigApplicationContext(EmpleadosConfig.class);
 
 		Empleados Antonio = contexto.getBean("comercialExperimentado", Empleados.class);
 		Empleados Lucia = contexto.getBean("comercialExperimentado", Empleados.class);
